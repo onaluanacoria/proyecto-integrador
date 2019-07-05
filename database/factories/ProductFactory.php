c7166aac939f831be512dc6ac9230ae06f94c583
onaluanacoria <onaluanacoria@hotmail.com>
2019-07-05 14:15:02 -0300

    con migraciones

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,16 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\Product;
+use Faker\Generator as Faker;
+
+$factory->define(Product::class, function (Faker $faker) {
+  $dir= storage_path('app/public/product');
+    return [
+      'name'=>$faker->word(3),
+      'description'=>$faker->sentence(6),
+      'price'=> $faker->randomFloat(2,300, 4000),
+      "featured_img"=>$faker->image($dir, 300, 300, 'cats', false)
+    ];
+});
---
+++ database/migrations/2019_07_03_181530_create_products_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+          //Campos de la tabla
+            $table->bigIncrements('id');
+            $table->timestamps();
+            $table->string('name')->lenght('100');
+            $table->string('description');
+            $table->decimal('price');
+            $table->string('featured_img');
+            $table->bigInteger('categoria_id')->unsigned()->nullable();
+            //UNSIGNEDBIGINTEGER TAMBIEN PUEDE SER
+
+            $table->foreign('categoria_id')->references('id')->on('categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+        $table->dropForeign('categoria_id');
+    }
+}
